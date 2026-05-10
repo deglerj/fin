02bd31e4edce07e58064ac2fa9d3d0b633b7c3f2
Johannes Degler <johannes@degler-online.de>
2026-05-10 21:10:51 +0200

    chore: scaffold project structure and dependencies

---
+++ cmd/fin/main.go
@@ -0,0 +1,17 @@
+// cmd/fin/main.go
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(nil, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
